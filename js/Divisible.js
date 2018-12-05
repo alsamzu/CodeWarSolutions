d38f5d5fc6bbd22c3cec819838c21ad4146df45a
alsamzu <zulfalsam@gmail.com>
2018-12-05 21:14:18 +0300

    CodeWars Solutions

--- js/Divisible.js
+++ js/Divisible.js
@@ -1,11 +1,14 @@
 // Create a function isDivisible(n, x, y) that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
 
 function IsDivisible(n,x,y){
+
     if(n % x ==0 && n % y ==0){
-        return true;
+        console.log(n+" is divisible by " +x+" and "+y)
+       // return true;
     }
     else{
-        return false;
+        console.log(n+" is not divisible by " +x+" and "+y)
+       // return false;
     }
 }
-IsDivisible(12,3,4);
+IsDivisible(12,5,4);
